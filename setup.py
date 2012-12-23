f015f1e64f11c856a3b5615fd33c6032729ed407
Grigory Petrov <grigory.v.p@gmail.com>
2012-12-24 02:12:38 +0400

    Info updated.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
   },
   ##  http://pypi.python.org/pypi?:action=list_classifiers
   classifiers  = [
-    'Development Status :: 1 - Planning',
+    'Development Status :: 2 - Pre-Alpha',
     'Environment :: Console',
     'Intended Audience :: End Users/Desktop',
     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
