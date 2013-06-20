d2dd8e380877165c237b45bff46fb5870ed55173
Grigory Petrov <grigory.v.p@gmail.com>
2013-06-20 23:32:35 +0400

    Refactoring for updated coding standard.

--- pywincmdtheme/__init__.py
+++ pywincmdtheme/__init__.py
@@ -14,64 +14,64 @@ from win32com.shell import shell, shellcon
 import pythoncom
 
 
-def main() :
+def main():
 
   sPath = os.path.join( os.path.expanduser( "~" ), ".Xresources" )
   mColors = {}
-  for i in range( 16 ) :
+  for i in range( 16 ):
     mColors[ i ] = 0
-  try :
-    with open( sPath ) as oFile :
-      for sLine in oFile :
+  try:
+    with open( sPath ) as oFile:
+      for sLine in oFile:
         ## *background: #300a24
         sRe = r'.+background\s*:\s*#(?P<color>[\da-fA-F]+)'
         oMatch = re.match( sRe, sLine )
-        if oMatch :
+        if oMatch:
           mMatch = oMatch.groupdict()
           mColors[ 'background' ] = int( mMatch[ 'color' ], 16 )
         sRe = r'.+foreground\s*:\s*#(?P<color>[\da-fA-F]+)'
         oMatch = re.match( sRe, sLine )
-        if oMatch :
+        if oMatch:
           mMatch = oMatch.groupdict()
           mColors[ 'foreground' ] = int( mMatch[ 'color' ], 16 )
         sRe = r'.+color(?P<id>\d+)\s*:\s*#(?P<color>[\da-fA-F]+)'
         oMatch = re.match( sRe, sLine )
-        if oMatch :
+        if oMatch:
           mMatch = oMatch.groupdict()
           mColors[ int( mMatch[ 'id' ] ) ] = int( mMatch[ 'color' ], 16 )
-  except IOError :
+  except IOError:
     print( "~/.Xresources not found, using Ubuntu terminal color theme." )
     mColors = {
-      0 : 0x300a24,
-      1 : 0xcc0000,
-      2 : 0x4e9a06,
-      3 : 0xc4a000,
-      4 : 0x3465a4,
-      5 : 0x75507b,
-      6 : 0x06989a,
-      7 : 0xd3d7cf,
-      8 : 0x555753,
-      9 : 0xef2929,
-      10 : 0x8ae234,
-      11 : 0xfce94f,
-      12 : 0x729fcf,
-      13 : 0xad7fa8,
-      14 : 0x34e2e2,
-      15 : 0xeeeeec,
+      0: 0x300a24,
+      1: 0xcc0000,
+      2: 0x4e9a06,
+      3: 0xc4a000,
+      4: 0x3465a4,
+      5: 0x75507b,
+      6: 0x06989a,
+      7: 0xd3d7cf,
+      8: 0x555753,
+      9: 0xef2929,
+      10: 0x8ae234,
+      11: 0xfce94f,
+      12: 0x729fcf,
+      13: 0xad7fa8,
+      14: 0x34e2e2,
+      15: 0xeeeeec,
     }
 
   ##  Windows don't have separate backgound and foreground colors.
-  if 'background' in mColors :
+  if 'background' in mColors:
     mColors[ 0 ] = mColors[ 'background' ]
     del mColors[ 'background' ]
-  if 'foreground' in mColors :
+  if 'foreground' in mColors:
     mColors[ 7 ] = mColors[ 'foreground' ]
     del mColors[ 'foreground' ]
 
   ##  Colors is .Xresources are defined as "RGB" text, and while converting
   ##  in number that became 0xRGB, and we need red to be less significant,
   ##  so convert to 0xBGR
-  for nKey, nVal in mColors.items() :
+  for nKey, nVal in mColors.items():
     nVal = ((nVal >> 16) & 0xFF) + (nVal & 0xFF00) + ((nVal << 16) & 0xFF0000)
     mColors[ nKey ] = int( nVal )
 
@@ -86,8 +86,8 @@ def main() :
   mColors[ 12 ] = mColorsOld[ 9 ]
   mColors[ 14 ] = mColorsOld[ 11 ]
 
-  if len( sys.argv ) > 1 and '--update-link' in sys.argv[ 1 ] :
-    if len( sys.argv ) < 3 :
+  if len( sys.argv ) > 1 and '--update-link' in sys.argv[ 1 ]:
+    if len( sys.argv ) < 3:
       print( "Error: Path to .lnk file not specified." )
       exit( 1 )
     oLink = pythoncom.CoCreateInstance( shell.CLSID_ShellLink, None,
@@ -98,7 +98,7 @@ def main() :
     oLinkData = oLink.QueryInterface( shell.IID_IShellLinkDataList )
     oBlock = oLinkData.CopyDataBlock( shellcon.NT_CONSOLE_PROPS_SIG )
     lColors = []
-    for i in range( 16 ) :
+    for i in range( 16 ):
       lColors.append( mColors[ i ] )
     oBlock[ 'ColorTable' ] = lColors
     ##  Background color( 0 ) and foreground color( 7, white  ). Windows
@@ -108,11 +108,11 @@ def main() :
     oLinkData.RemoveDataBlock( shellcon.NT_CONSOLE_PROPS_SIG )
     oLinkData.AddDataBlock( oBlock )
     oPersist.Save( sFile, True )
-  else :
+  else:
     nGroup = _winreg.HKEY_CURRENT_USER
     nRight = _winreg.KEY_ALL_ACCESS
-    with _winreg.OpenKey( nGroup, 'Console', 0, nRight ) as oKey :
-      for nKey, nVal in mColors.items() :
+    with _winreg.OpenKey( nGroup, 'Console', 0, nRight ) as oKey:
+      for nKey, nVal in mColors.items():
         sName = 'ColorTable{:02d}'.format( nKey )
         _winreg.SetValueEx( oKey, sName, 0, _winreg.REG_DWORD, nVal )
     print( """
